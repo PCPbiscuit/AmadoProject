29a1f2ba21abd015e3d0007f1ed59bd1ad1b6d76
PCPbiscuit <57859010+PCPbiscuit@users.noreply.github.com>
2020-07-01 21:06:46 +0300

    Updates

---
+++ precache-manifest.82a24cb8e4420b84de8761ed18e2b886.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64ee04c3685b386a273c29dac9c069c",
+    "url": "/AmadoProject/index.html"
+  },
+  {
+    "revision": "32cb0d2e489872d69882",
+    "url": "/AmadoProject/static/css/main.cc241e72.chunk.css"
+  },
+  {
+    "revision": "3a452211a0a4c1235b49",
+    "url": "/AmadoProject/static/js/2.93ef19ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AmadoProject/static/js/2.93ef19ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4885127a22140f1aef3d",
+    "url": "/AmadoProject/static/js/3.c6203a89.chunk.js"
+  },
+  {
+    "revision": "aac51b0062f6140371b8",
+    "url": "/AmadoProject/static/js/4.6d763f5e.chunk.js"
+  },
+  {
+    "revision": "43745f186e4e885b1e79",
+    "url": "/AmadoProject/static/js/5.2306fb81.chunk.js"
+  },
+  {
+    "revision": "2d5bc24552c07a7f8db5",
+    "url": "/AmadoProject/static/js/6.76b889ff.chunk.js"
+  },
+  {
+    "revision": "7112bdd5d72779815e54",
+    "url": "/AmadoProject/static/js/7.194397f9.chunk.js"
+  },
+  {
+    "revision": "32cb0d2e489872d69882",
+    "url": "/AmadoProject/static/js/main.f0c1859c.chunk.js"
+  },
+  {
+    "revision": "45865c374d69c1151158",
+    "url": "/AmadoProject/static/js/runtime-main.1657bcda.js"
+  },
+  {
+    "revision": "dbffe2bf4bc0eabb6a05b7a9c2ba0807",
+    "url": "/AmadoProject/static/media/pro-big-1.dbffe2bf.jpg"
+  },
+  {
+    "revision": "4c1f54df81a3295fdb65541e7f7309e0",
+    "url": "/AmadoProject/static/media/pro-big-2.4c1f54df.jpg"
+  },
+  {
+    "revision": "b0a32507d30aad5a6014c53d5adc24d7",
+    "url": "/AmadoProject/static/media/pro-big-3.b0a32507.jpg"
+  },
+  {
+    "revision": "1ea8966c43cc2f1091c2216b19c71c54",
+    "url": "/AmadoProject/static/media/pro-big-4.1ea8966c.jpg"
+  }
+]);
